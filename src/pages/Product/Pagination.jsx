ae53c741dc2402b34391b09ed18b658bfef0c547
MACMINI-DIT03 <>
2022-10-12 17:05:23 +0800

    no message

--- src/pages/Product/Pagination.jsx
+++ src/pages/Product/Pagination.jsx
@@ -1,17 +1,62 @@
-import { useMemo, useState } from "react";
+import React from "react";
+import { usePagination, DOTS } from "./usePagination";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+const Pagination = (props) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+    className,
+  } = props;
 
-export const usePagination = (data=[], volume = 10) => {
-  /** All pages in total. */
-  const totalPages = useMemo(
-    () => Math.floor(data.length / volume),
-    [volume, data.length]
-  );
-  const [page, setPage] = useState(0);
-  /** Data representing one single page. */
-  const slicedData = useMemo(
-    () => data.slice(page * volume, page * volume + volume),
-    [volume, page]
-  );
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
 
-  return { data: slicedData, page, totalPages, setPage };
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  const lastPage = paginationRange[paginationRange.length - 1];
+  return (
+    <div className={`pagination ${className}`}>
+      <button className={"pagination__btn"} onClick={onPrevious}>
+        <MdKeyboardArrowLeft className="icon" />
+      </button>
+      {paginationRange.map((pageNumber) => {
+        if (pageNumber === DOTS) {
+          return <span className="pagination__text">&#8230;</span>;
+        }
+
+        return (
+          <button
+            className={`pagination__btn ${
+              pageNumber === currentPage ? "pagination__btn--active" : ""
+            }`}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        );
+      })}
+      <button className={"pagination__btn"} onClick={onNext}>
+        <MdKeyboardArrowRight className="icon" />
+      </button>
+    </div>
+  );
 };
+
+export default Pagination;
